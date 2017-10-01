3c8127f365cc5a42da39cc23b8025fa876e41557
WyldePointer <sohrab.monfared@gmail.com>
2017-10-01 11:08:15 +0330

    Performance improvement.

--- php/arabic_numbers_to_latin.php
+++ php/arabic_numbers_to_latin.php
@@ -4,11 +4,13 @@ function arabic_numbers_to_latin($string){
 
   $latin = NULL;
 
+  $length = mb_strlen($string, "UTF-8");
+
   $numbers = array(
     '۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'
   );
 
-  for($i=0; $i < mb_strlen($string, "UTF-8"); $i++){
+  for($i=0; $i < $length; $i++){
     $latin .= array_search(mb_substr($string, $i, 1, "UTF-8"), $numbers);
   } 
 
